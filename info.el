4783f93f502cb2ee58d820603bf607eb5153db09
Wes Morgan <cap10morgan@gmail.com>
2014-02-05 19:55:22 -0700

    bump version to 1.0.3

--- info.el
+++ info.el
@@ -1,3 +1,3 @@
 (live-pack-name "rails-pack")
-(live-pack-version "1.0.2")
+(live-pack-version "1.0.3")
 (live-pack-description "Adds additional features for Ruby on Rails development. You almost certainly want ruby-pack too.")
